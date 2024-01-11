52871433ea9e5aa1c3290a3bda1065856fa7237a
bkl <dev.bkl.13@gmail.com>
2024-01-11 07:21:26 +0700

    Move smartlook tracking code to separated file

---
+++ smartlook-tracking.js
@@ -0,0 +1,6 @@
+window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+})(document);
+smartlook('init', '3ffc0dbbdc40f97b244663f1104281fb9d856a34', { region: 'eu' });
